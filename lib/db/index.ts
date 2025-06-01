1cf7a727a883a4f0b7233b08af165e84903c500d
Dat Gia Truong <datgiatruong@192.168.2.31>
2025-06-02 00:05:07 +0700

    feat: db setup

---
+++ lib/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+import env from "~/lib/env";
+
+import * as schema from "./schema";
+
+export default drizzle({ connection: {
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
+}, schema, casing: "snake_case" });
