2e5a0109cb0188d13e0f8850d2508d316f5ce493
Dat Gia Truong <datgiatruong@192.168.2.31>
2025-06-04 17:47:33 +0700

    feat: logged in user

---
+++ pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="bg-base-300 card container mx-auto mt-4 text-center min-h-72 flex flex-col justify-center items-center gap-4">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
