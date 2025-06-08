0298b64ed24985c3e816584d6ded6b6a7b9d2b7a
Dat Gia Truong <datgiatruong@192.168.2.31>
2025-06-08 14:45:47 +0700

    feat: create new location

--- components/sidebar-button.vue
+++ components/sidebar-button.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const props = defineProps<{
+defineProps<{
   label: string;
   icon: string;
   href: string;
@@ -10,12 +10,12 @@ const route = useRoute();
 </script>
 
 <template>
-  <div class="tooltip tooltip-right" :data-tip="props.showLabel ? undefined : props.label">
-    <NuxtLink :to="props.href" :class="{ 'bg-base-200': route.path === props.href, 'justify-center': !props.showLabel, 'justify-start': props.showLabel }" class="flex gap-2 p-2 hover:bg-base-300 cursor-pointer flex-nowrap">
-      <Icon :name="props.icon" size="24" />
+  <div class="tooltip tooltip-right" :data-tip="showLabel ? undefined : label">
+    <NuxtLink :to="href" :class="{ 'bg-base-200': route.path === href, 'justify-center': !showLabel, 'justify-start': showLabel }" class="flex gap-2 p-2 hover:bg-base-300 cursor-pointer flex-nowrap">
+      <Icon :name="icon" size="24" />
       <Transition>
-        <span v-if="props.showLabel">
-          {{ props.label }}
+        <span v-if="showLabel">
+          {{ label }}
         </span>
       </Transition>
     </NuxtLink>
