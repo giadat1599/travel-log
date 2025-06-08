1d7d1c6cb0dd7f2174aec3b8a55d98f0cdb0aee2
Dat Gia Truong <truonggiadat15@gmail.com>
2025-06-08 14:48:31 +0700

    feat: create new location (#13)

---
+++ components/app/form-field.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean;
+  error?: string;
+  label: string;
+  name: string;
+  type?: "text" | "textarea" | "number";
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ label }}
+    </legend>
+    <Field
+      :disabled="disabled"
+      :as="type === 'textarea' ? 'textarea' : 'input'"
+      :name="name"
+      :type="type || 'text'"
+      class="w-full"
+      :class="{ 'input-error': error, 'input': !type || ['text', 'number'].includes(type), 'textarea': type === 'textarea' }"
+    />
+    <p v-if="error" class="label" :class="{ 'text-error': error }">
+      {{ error }}
+    </p>
+  </fieldset>
+</template>
