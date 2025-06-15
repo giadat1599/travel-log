684d14c572960a510066cb8041ab7534a8673e2b
Dat Gia Truong <truonggiadat15@gmail.com>
2025-06-15 23:38:30 +0700

    feat: show locations on map (#18)

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
+import type { LngLatLike } from "maplibre-gl";
+
 const colorMode = useColorMode();
-const center = ref<[number, number]>([-1.559482, 47.21322]);
+const mapStore = useMapStore();
+const center = ref<LngLatLike>([-1.559482, 47.21322]);
 
 const style = computed(() => {
   return colorMode.value === "dark"
@@ -10,19 +13,35 @@ const style = computed(() => {
 const zoom = 4;
 
 onMounted(async () => {
-  const location = await getCurrentLocation();
-  if (location) {
+  if (mapStore.mapPoints.length === 0) {
+    const location = await getCurrentLocation();
     center.value = [location.longitude, location.latitude];
+  }
+  else {
+    await mapStore.init();
   }
 });
 </script>
 
 <template>
-  <MglMap
-    :map-style="style"
-    :center="center"
-    :zoom="zoom"
-  >
-    <MglNavigationControl />
-  </MglMap>
+  <div class="p-4">
+    <MglMap
+      :map-style="style"
+      :center="center"
+      :zoom="zoom"
+    >
+      <MglNavigationControl />
+      <MglMarker
+        v-for="point in mapStore.mapPoints"
+        :key="point.id"
+        :coordinates="[point.long, point.lat]"
+      >
+        <template #marker>
+          <div class="tooltip tooltip-top" :data-tip="point.label">
+            <Icon name="tabler:map-pin-filled" size="30" class="text-secondary" />
+          </div>
+        </template>
+      </MglMarker>
+    </MglMap>
+  </div>
 </template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -19,7 +19,7 @@ function toggleSidebar() {
 
 <template>
   <div class="flex-1 flex">
-    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+    <div class="bg-base-100 transition-all duration-300 shrink-0" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
       <div class="flex cursor-pointer hover:bg-base-200 p-2" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toggleSidebar">
         <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
         <Icon v-else name="tabler:chevron-right" size="32" />
@@ -60,9 +60,11 @@ function toggleSidebar() {
         />
       </div>
     </div>
-    <div class="flex-1 flex flex-col">
-      <NuxtPage />
-      <AppMap class="flex-1" />
+    <div class="flex-1 overflow-auto">
+      <div class="flex flex-col size-full">
+        <NuxtPage />
+        <AppMap class="flex-1" />
+      </div>
     </div>
   </div>
 </template>
