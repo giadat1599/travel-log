fa2acccad2c0380261ecb61df6cef8fd8ab3787f
Dat Gia Truong <datgiatruong@Dats-MacBook-Pro-2.local>
2025-07-19 19:55:30 +0700

    feat: location logs

--- stores/sidebar.ts
+++ stores/sidebar.ts
@@ -1,11 +1,14 @@
+import type { RouteLocationRaw } from "vue-router";
+
 import type { MapPoint } from "~/lib/types";
 
 export type SideBarItem = {
   id: string;
   label: string;
   icon: string;
-  href: string;
-  location?: MapPoint | null;
+  href?: string;
+  mapPoint?: MapPoint | null;
+  to?: RouteLocationRaw;
 };
 
 export const useSidebarStore = defineStore("useSideBarStore", () => {
